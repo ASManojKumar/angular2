ee21882a9f7a0ab3d28f2c6c5ad6f572182e82e5
Manoj Kumar AS <manoj.as@tavant.com>
2017-02-27 12:40:55 +0530

    added cdn links

--- app/app.hero.js
+++ app/app.hero.js
@@ -1,19 +1,11 @@
 (function(app) {
     app.Hero = Hero;
 
-    app.Hero1 = Hero1;
-
     function Hero(name, dob, age, zipcode, city) {
         this.name = name;
         this.dob = dob;
         this.age = age;
         this.city = city;
         this.zipcode = zipcode;
-    }
-
-    function Hero1(name, dob, age) {
-        this.name = name;
-        this.dob = dob;
-        this.age = age;
     }
 })(window.app || (window.app = {}));
